714c1161e554498cf85a210e4ca8a0c2ad0e5588
Mark Graham <markgraham539@gmail.com>
2023-02-01 16:47:15 -0600

    Train with monai generative

---
+++ docker/create_docker_image_runai.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# A simple script to build the distributed Docker image.
+#
+# $ create_docker_image.sh
+set -ex
+TAG=ddpm-ood
+
+# get monai generative
+git clone git@github.com:Project-MONAI/GenerativeModels.git
+
+cp ../requirements.txt .
+docker build --network=host --tag "aicregistry:5000/${USER}:${TAG}" . \
+  --build-arg USER_ID=$(id -u) \
+  --build-arg GROUP_ID=$(id -g) \
+  --build-arg USER=${USER}
+
+#push
+docker push "aicregistry:5000/${USER}:${TAG}"
