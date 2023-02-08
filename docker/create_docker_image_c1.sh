6f151015761b31e2ab49a3f2d00990221cb14a4b
Mark Graham <markgraham539@gmail.com>
2023-02-08 14:07:53 -0600

    C1 image

---
+++ docker/create_docker_image_c1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# A simple script to build the distributed Docker image.
+#
+# $ create_docker_image.sh
+set -ex
+TAG=nvcr.io/r5nte7msx1tj/amigo/ddp-ood:v0.1.0
+
+# get monai generative
+git clone git@github.com:Project-MONAI/GenerativeModels.git
+
+cp ../requirements.txt .
+docker build --network=host --tag "${TAG}" . \
+  --build-arg USER_ID=$(id -u) \
+  --build-arg GROUP_ID=$(id -g) \
+  --build-arg USER=${USER}
+
+#push
+docker push "${TAG}"
